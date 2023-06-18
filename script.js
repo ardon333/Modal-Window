0955c51704e87eeadf0014787f24ee81bff6023d
Ardon Kotey <koteyardon2002@gmail.com>
2023-06-18 14:41:21 +0530

    Modal window

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const closeBtn=document.getElementById("close-btn");
 
 openBtn.addEventListener("click",function(e){
      modalContainer.style.display="inline-block";
-})
+},function(e){console.log("hello world")});
 
 closeBtn.addEventListener("click",function(e){
     modalContainer.style.display="none";
@@ -14,3 +14,28 @@ window.addEventListener("click",function(e){
     if(e.target===modalContainer)
     modalContainer.style.display="none";
 })
+
+
+
+
+// const node1=document.querySelectorAll("li");
+// const obj1={
+//     0:"This is a line",
+//     1: 10,
+//     2: "Hello world",
+//     length:3
+// }
+
+// console.log(document.querySelector("#modal"))
+// console.log(Array.from(obj1));
+// console.log(node1);
+// console.log(`Type:${typeof node1}`)
+
+// const list1=document.querySelector(".list");
+
+// const message=document.createElement("div");
+
+// message.innerHTML="This has been added by javascript <br><button>LINK UP</button>"
+
+
+
---
+++ script2.js
@@ -0,0 +1,14 @@
+const obj1={
+    0:"first",
+    1:"second",
+    2:"third",
+    length:3
+
+}
+
+console.log(Array.from(obj1));
+const list1=document.querySelector(".insidelist");
+console.log(Array.from(list1.children));
+
+
+console.log(list1.parentElement)
